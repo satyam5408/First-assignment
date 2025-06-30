76ef4e621c222b17fc2c6c03fc206a5ac472cb81
satyam5408 <satyamchaudhary540880@gmail.com>
2025-06-30 22:10:18 +0530

    Add files via upload

---
+++ first_assignment.py
@@ -0,0 +1,18 @@
+# First task
+
+num1 = int(input("enter the first number:"))
+num2 = int(input("enter the second number:"))
+
+print("addition:", num1 + num2)
+print("subtraction:", num1 - num2)
+print("multiplication:", num1 * num2)
+print("division:", num1 / num2)
+
+# Second task
+
+first = input("enter your first name:")
+last = input("enter your last name:")
+
+full_name = (first + last+"!")
+
+print("Hi,", full_name," welcome to the python program.")
